834156c510d11d1c53de8f647a0b6e8738220bb1
JefersonT4v4res <jefersonsaj_tavares@hotmail.com>
2025-10-03 20:51:13 -0300

    :feat: add homepage with custom CSS, reset styles, images and fonts

--- app/videos/routes.py
+++ app/videos/routes.py
@@ -2,6 +2,10 @@ from flask import Blueprint, render_template
 
 videos_bp = Blueprint("videos", __name__)
 
-@videos_bp.route("/")
-def index():
-    return render_template("videos.html")
+@videos_bp.route("/youtube-to-mp3")
+def youtube_to_mp3():
+    return render_template("youtubeToMp3.html")
+
+@videos_bp.route("/youtube-to-mp4")
+def youtube_to_mp4():
+    return render_template("youtubeToMp4.html")
